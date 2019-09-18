0492fb35164b1734bd474211a4165616e16a6a2a
Rahul Das <iamrahuldas001@gmail.com>
2019-09-18 06:49:32 +0530

    Add files via upload
    after copy src file in your project install all npm react-redux dependency

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// npm install axios redux react-redux redux-thunk react-router-dom 
+
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>HELLO RAHUL</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
